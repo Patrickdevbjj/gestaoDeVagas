99ac1725c7da78f66dcf7fc00ad338bbe1ee26a0
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-10-29 20:08:40 -0300

    Ajustando o regex e adicionando o NotBlank

--- src/main/java/br/com/patrick/gestao_vagas/exceptions/ExceptionHandlerController.java
+++ src/main/java/br/com/patrick/gestao_vagas/exceptions/ExceptionHandlerController.java
@@ -3,8 +3,14 @@ package br.com.patrick.gestao_vagas.exceptions;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
@@ -16,9 +22,17 @@ public class ExceptionHandlerController {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public void handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-         exception.getBindingResult().getFieldErrors().forEach(err -> {
+    public ResponseEntity<List<ErrorMessageDTO>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+         
+        List<ErrorMessageDTO> dto = new ArrayList<>();
+        
+        exception.getBindingResult().getFieldErrors().forEach(err -> {
             String message = messageSource.getMessage(err, LocaleContextHolder.getLocale());
+        ErrorMessageDTO error =  new ErrorMessageDTO(message, err.getField());
+        dto.add(error);
+        
         });
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    
     }
 }
--- src/main/java/br/com/patrick/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/patrick/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -13,7 +14,9 @@ public class CandidateEntity {
     
     private UUID id;
     private String name;
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
+    
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
     
     @Email(message = "O campo [email] deve conter um email válido")
