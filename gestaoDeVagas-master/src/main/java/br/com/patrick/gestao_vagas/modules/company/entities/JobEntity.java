de252b861fc5995a9c6ba31aa84f1731ed3c8bc0
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-11-01 02:53:41 -0300

    Atualize jobEntity

--- gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/company/entities/JobEntity.java
+++ gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,9 +27,10 @@ public class JobEntity {
 
 
     @ManyToOne()
-    @JoinColumn(name = "company_id")
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
 
+    @Column(name = "company_id")
     private UUID companyId;
 
     @CreationTimestamp
