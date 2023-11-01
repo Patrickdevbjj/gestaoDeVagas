7e27317d03796a732c9a3e72602636ea18e1e236
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-11-01 01:45:58 -0300

    Add a new layer for my message error

---
+++ gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,35 @@
+package br.com.patrick.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.patrick.gestao_vagas.exceptions.UserFoundException;
+import br.com.patrick.gestao_vagas.modules.UseCases.CreateCandidateUseCase;
+import br.com.patrick.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+
+
+@RestController
+@RequestMapping("/candidate")
+@RequiredArgsConstructor
+public class CandidateController {
+    
+    @Autowired
+    private CreateCandidateUseCase createCandidateUseCase;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
