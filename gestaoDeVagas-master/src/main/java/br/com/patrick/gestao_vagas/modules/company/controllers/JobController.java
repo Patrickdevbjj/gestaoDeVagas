6dcb72ed5342e304c0f1bb6de7a36781cb263d53
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-11-01 03:53:49 -0300

    create a file jobController

---
+++ gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/company/controllers/JobController.java
@@ -0,0 +1,23 @@
+package br.com.patrick.gestao_vagas.modules.company.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.patrick.gestao_vagas.modules.UseCases.CreateJobUseCase;
+import br.com.patrick.gestao_vagas.modules.company.entities.JobEntity;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+    @Autowired
+    private CreateJobUseCase createJobUseCase;
+
+    public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
+        return this.createJobUseCase.execute(jobEntity);
+    }
+
+}
