5b994f34990e93a204419da0eebfc801427bb0fa
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-11-01 03:06:17 -0300

    fixed a problem in last file add

--- gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/company/repositories/CompanyRepository.java
+++ gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -1,11 +1,14 @@
 package br.com.patrick.gestao_vagas.modules.company.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.patrick.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.patrick.gestao_vagas.modules.company.entities.CompanyEntity;
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
-    
+    Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+
 }
