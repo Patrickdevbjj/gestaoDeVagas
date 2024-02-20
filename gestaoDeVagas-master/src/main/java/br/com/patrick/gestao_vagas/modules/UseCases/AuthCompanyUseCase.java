872af0eedc145ffd464d32b44f831dc73d6c8ca9
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-02-20 11:34:38 -0300

    updating AuthCompany

--- gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/UseCases/AuthCompanyUseCase.java
+++ gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/UseCases/AuthCompanyUseCase.java
@@ -1,26 +1,38 @@
 package br.com.patrick.gestao_vagas.modules.UseCases;
 
+import javax.naming.AuthenticationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.patrick.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.patrick.gestao_vagas.modules.company.repositories.CompanyRepository;
+import lombok.var;
 
 @Service
 public class AuthCompanyUseCase {
+   
     
     @Autowired
     private CompanyRepository companyRepository;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername())
-        .orElseThrow(() -> {
-            throw new UsernameNotFoundException("User not found");
-            
-        });
-        
+
+    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername())
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    
+
+    // Verificar a senha são iguais
+    var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+    // Se não for igual -> Erro
+    if (!passwordMatches) {
+        throw new AuthenticationException();
     }
-
+    }
 }
