bb537a5d8cf4ba395c9ba8f1d02f7db93f1e2062
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-02-22 11:39:27 -0300

    password hash

--- gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/UseCases/CreateCandidateUseCase.java
+++ gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/UseCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.patrick.gestao_vagas.modules.UseCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.patrick.gestao_vagas.exceptions.UserFoundException;
@@ -13,13 +14,18 @@ public class CreateCandidateUseCase {
    @Autowired
     private CandidateRepository candidateRepository;
 
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent((c) -> {
             throw new UserFoundException();
         });
+        
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+        
         return this.candidateRepository.save(candidateEntity);
 
     }
